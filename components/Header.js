1ff98b339ff659797fe9654b47adb32b39858f8d
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-03-04 19:11:48 -0700

    Add home button

--- components/Header.js
+++ components/Header.js
@@ -28,6 +28,27 @@ const NavBar = () => {
   );
 };
 
+const getRandomEmoji = () => {
+  const options = [
+    "💙",
+    "🐈‍⬛",
+    "🎸",
+    "🎮",
+    "👾",
+    "📖",
+    "📝",
+    "🤖",
+    "🪴",
+    "🏋️",
+    "🚴",
+    "🎧",
+    "🎺",
+    "🪕",
+    "🎼",
+  ];
+  return options[Math.floor(Math.random() * options.length)];
+};
+
 const Header = ({ navBarTitle, fullWidth }) => {
   const useSticky = !BLOG.autoCollapsedNavBar;
   const navRef = useRef(null);
@@ -43,6 +64,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
       navRef.current?.classList.add("remove-sticky");
     }
   };
+
   useEffect(() => {
     const obvserver = new window.IntersectionObserver(handler);
     obvserver.observe(sentinalRef.current);
@@ -65,34 +87,8 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/">
             <a aria-label={BLOG.title}>
-              <div className="h-6">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    width="24"
-                    height="24"
-                    className="fill-current text-black dark:text-white"
-                  />
-                  <rect width="24" height="24" fill="url(#paint0_radial)" />
-                  <defs>
-                    <radialGradient
-                      id="paint0_radial"
-                      cx="0"
-                      cy="0"
-                      r="1"
-                      gradientUnits="userSpaceOnUse"
-                      gradientTransform="rotate(45) scale(39.598)"
-                    >
-                      <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                      <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                    </radialGradient>
-                  </defs>
-                </svg>
+              <div style={{ scale: "2.3", paddingLeft: "1rem" }}>
+                🏠{getRandomEmoji()}
               </div>
             </a>
           </Link>
