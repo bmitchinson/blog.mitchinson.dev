9b5a08330dd21d12d2d162d85bd6fa5b21f1e1c3
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-03-29 19:20:47 -0600

    Enable sort by date + disable (useless? archive boolean)

--- blog.config.js
+++ blog.config.js
@@ -13,8 +13,7 @@ const BLOG = {
   path: "", // leave this empty unless you want to deploy Nobelium in a folder
   since: 2017, // If leave this empty, current year will be used.
   postsPerPage: 6,
-  sortByDate: false,
-  showArchive: true,
+  sortByDate: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: "https://og-image-craigary.vercel.app", // The link to generate OG image, don't end with a slash
   socialLink: "https://twitter.com/115bwm",
