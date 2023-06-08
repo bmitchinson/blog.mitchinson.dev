4a1bc93a0904dbd72c46b5bc208b2d75214ce876
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-06-07 19:52:19 -0500

    Enable Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "katex/dist/katex.min.css";
 import "@/styles/globals.css";
 import "@/styles/notion.css";
 import { LocaleProvider } from "@/lib/locale";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
       <LocaleProvider>
         <>
           <Component {...pageProps} />
+          <Analytics />
         </>
       </LocaleProvider>
     </>
